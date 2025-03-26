8406f3a6a0411d3cb25ad7adff566e53a538400e
CodeBear <249900679@qq.com>
2025-03-26 14:21:25 +0700

    update

--- packages/updater/ios/Sources/OukekUpdater/OukekUpdater.m
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(OukekUpdater, "OukekUpdater",
-    CAP_PLUGIN_METHOD(getCurrentVersion, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(getStoreVersion, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(openAppStore, CAPPluginReturnPromise);
-)
